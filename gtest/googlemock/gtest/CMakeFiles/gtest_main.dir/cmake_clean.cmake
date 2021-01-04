121ec4bf41b7c765dee3005dbc59e82772189bdc
SHARKKK7 <61628765+SHARKKK7@users.noreply.github.com>
2021-01-05 01:29:03 +0300

    Add files via upload

---
+++ gtest/googlemock/gtest/CMakeFiles/gtest_main.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/gtest_main.dir/src/gtest_main.cc.o"
+  "/home/sharkkk/ros_lab/lab4/lib/libgtest_main.pdb"
+  "/home/sharkkk/ros_lab/lab4/lib/libgtest_main.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/gtest_main.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
